dcac4dfe473c2445ef1e4ce2675f336521fea9cd
VidhyaSakthivelan <152424242+VidhyaSakthivelan@users.noreply.github.com>
2026-06-29 09:42:19 +0530

    Time: 136 ms (5.39%), Space: 48 MB (19.92%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public void moveZeroes(int[] nums) {
+      int i = 0;
+      int j = 0;
+      while(j<nums.length)
+      {
+        if(nums[j]!=0)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+        }
+        j++;
+      }
+      for (int val: nums) 
+      {
+        System.out.println(val+"");
+      }
+    }
+}
